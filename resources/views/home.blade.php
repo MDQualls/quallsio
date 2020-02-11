0ca6ce16b339ea6a1ad6693bf244168fc80a76ad
MDQualls <mqualls@gmail.com>
2020-02-10 20:23:03 -0600

    init styling of about section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,16 +32,42 @@
         <div class="row">
             <div class="nav-branding-area"><a href="#top">Qualls.IO</a></div>
             <ul class="main-nav-ul">
-                <li class="nav-item">About</li>
-                <li class="nav-item">Services</li>
-                <li class="nav-item">Timeline</li>
-                <li class="nav-item">Contact</li>
+                <li class="nav-item"><a href="#about-section">About Me</a></li>
+                <li class="nav-item"><a href="#services-section">Services</a></li>
+                <li class="nav-item"><a href="#timeline-section">Timeline</a></li>
+                <li class="nav-item"><a href="#contact-section">Contact</a></li>
             </ul>
         </div>
     </nav>
 
     <section id="about-section" class="about-section">
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="row">
+            <h2>About Me</h2>
+            <div class="about-text-box">
+                <div class="portrait-box">
+                    <img src="https://placeimg.com/350/450/any" alt="Portrait">
+                </div>
+                <div class="about-box">
+                    <p class="lead">
+                        Lorem Ipsum is simply dummy text of the printing
+                    </p>
+                    <p class="text">
+                        Lor1em Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                        and more recently with desktop.
+                    </p>
+                    <p class="text">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </p>
+                </div>
+            </div>
+        </div>
     </section>
 
     <section id="services-section" class="services-section">
